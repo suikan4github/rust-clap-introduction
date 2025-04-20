cca9fd19d57a2bd593b9420b02ae75af07dda9e6
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-04-21 06:27:35 +0900

    p0010-cliを追加

---
+++ p0010-cli/src/main.rs
@@ -0,0 +1,24 @@
+// clapクレートを使ってコマンドライン引数をパースするプログラム。
+// Cargo.tomlに以下を追加する。
+// [dependencies]
+// clap = { version = "4.0", features = ["derive"] }
+// clapのderive機能を使うために、クレートのバージョンを4.0以上にする必要がある。
+
+// clapクレートは、コマンドライン引数をパースするためのライブラリ。
+use clap::Parser;
+
+// derive(parser)属性を使ってコマンドライン引数のパースのためのコードを自動生成する。
+#[derive(Parser, Debug)]
+// command(version)属性を使って、コマンドラインに -V --version オプションを追加する。
+#[command(version)]
+// コマンドライン引数をパースするための構造体を定義する。
+// このプログラムではユーザー定義の構造体は空である。
+struct Cli {}
+
+fn main() {
+    // コマンドライン引数をパースする。
+    let cli = Cli::parse();
+
+    // コマンドライン引数をパースした結果を表示する。
+    println!("{:?}", cli);
+}
