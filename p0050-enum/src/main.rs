a3933f5a652de1af2f52237d3cda2d0f66f1081b
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-04-21 23:19:02 +0900

    p0050を追加

---
+++ p0050-enum/src/main.rs
@@ -0,0 +1,49 @@
+// clapクレートを使ってコマンドライン引数をパースするプログラム。
+// Cargo.tomlに以下を追加する。
+// [dependencies]
+// clap = { version = "4.0", features = ["derive"] }
+// clapのderive機能を使うために、クレートのバージョンを4.0以上にする必要がある。
+
+// clapクレートは、コマンドライン引数をパースするためのライブラリ。
+use clap::{Parser, ValueEnum};
+
+// enum型のコマンドライン引数を定義する。
+#[derive(Debug, Clone, ValueEnum)]
+enum EngineType {
+    Reciprocating,
+    Turboprop,
+    Turbojet,
+    Turbofan,
+}
+
+// derive(parser)属性を使ってコマンドライン引数のパースのためのコードを自動生成する。
+#[derive(Parser, Debug)]
+// command(version)属性を使って、コマンドラインに -V --version オプションを追加する。
+#[command(version)]
+// コマンドライン引数をパースするための構造体を定義する。
+// このプログラムではユーザー定義の構造体は空である。
+struct Cli {
+    // 省略できないコマンドライン文字列。
+    #[arg(required = true)]
+    name: String,
+
+    // 省略可能なコマンドライン文字列。
+    #[arg(short, long, default_value = "")]
+    manufacturer: String,
+
+    // 文字列以外のコマンドライン引数をパースする。
+    #[arg(short, long, default_value_t = 1904)]
+    first_flight_year: i32,
+
+    // enum型のコマンドライン引数をパースする。
+    #[arg(short, long, value_enum, default_value_t = EngineType::Reciprocating)]
+    engine_type: EngineType,
+}
+
+fn main() {
+    // コマンドライン引数をパースする。
+    let cli = Cli::parse();
+
+    // コマンドライン引数をパースした結果を表示する。
+    println!("{:?}", cli);
+}
