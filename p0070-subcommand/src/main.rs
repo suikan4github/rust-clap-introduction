3f7a81586ed2a117ed0458192a93aee2770b30ec
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-04-22 22:52:18 +0900

    p0070 期待通り動いた

--- p0070-subcommand/src/main.rs
+++ p0070-subcommand/src/main.rs
@@ -5,7 +5,7 @@
 // clapのderive機能を使うために、クレートのバージョンを4.0以上にする必要がある。
 
 // clapクレートは、コマンドライン引数を解析するためのライブラリ。
-use clap::{Parser, ValueEnum};
+use clap::{Parser, Subcommand, ValueEnum};
 
 // enum型のコマンドライン引数を定義する。
 #[derive(Debug, Clone, ValueEnum)]
@@ -21,35 +21,81 @@ enum EngineType {
 // command(version)属性を使って、コマンドラインに -V --version オプションを追加する。
 #[command(version)]
 // コマンドライン引数を解析するための構造体を定義する。
-// このプログラムではユーザー定義の構造体は空である。
 struct Cli {
-    // 省略できないコマンドライン文字列。
-    #[clap(help = "Name of airclaft")]
-    name: String,
+    // コマンドライン引数のサブコマンドを定義する。
+    #[clap(subcommand)]
+    command: Commands,
+}
 
-    // 省略可能なコマンドライン文字列。
-    #[clap(short, long, default_value = "", help = "Manufacturer of airclaft")]
-    manufacturer: String,
+// derive(subcommand)属性を使って、コマンドライン引数の解析のためのコードを自動生成する。
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Real {
+        // 省略できないコマンドライン文字列。
+        #[clap(help = "Name of airclaft")]
+        name: String,
 
-    // 文字列以外のコマンドライン引数を解析する。
-    #[clap(
-        short,
-        long,
-        default_value_t = 1904,
-        help = "First flight year of airclaft"
-    )]
-    first_flight: i32,
+        // 省略可能なコマンドライン文字列。
+        #[clap(short, long, default_value = "", help = "Manufacturer of airclaft")]
+        manufacturer: String,
 
-    // enum型のコマンドライン引数を解析する。
-    #[clap(short, long, value_enum, default_value_t = EngineType::Reciprocating,
+        // 文字列以外のコマンドライン引数を解析する。
+        #[clap(
+            short,
+            long,
+            default_value_t = 1904,
+            help = "First flight year of airclaft"
+        )]
+        first_flight: i32,
+
+        // enum型のコマンドライン引数を解析する。
+        #[clap(short, long, value_enum, default_value_t = EngineType::Reciprocating,
         help = "Engine type")]
-    engine_type: EngineType,
+        engine_type: EngineType,
+
+        // 論理型のコマンドライン引数を解析する。このオプションはスイッチとして機能する。
+        #[clap(short, long, help = "Specify pretty print mode")]
+        pretty_print: bool,
+    },
+    Idea {
+        // 省略できないコマンドライン文字列。
+        #[clap(help = "Name of airclaft")]
+        name: String,
+
+        // 省略可能なコマンドライン文字列。
+        #[clap(short, long, default_value = "", help = "Designer of airclaft")]
+        designer: String,
+    },
 }
 
 fn main() {
     // コマンドライン引数を解析する。
     let cli = Cli::parse();
 
-    // コマンドライン引数を解析した結果を表示する。
-    println!("{:?}", cli);
+    match cli.command {
+        Commands::Real {
+            name,
+            manufacturer,
+            first_flight,
+            engine_type,
+            pretty_print,
+        } => {
+            if pretty_print {
+                // コマンドライン引数をきれいに表示する。
+                println!("Name         {:#?}", name);
+                println!("Manufacturer {:#?}", manufacturer);
+                println!("First flight {:#?}", first_flight);
+                println!("Engine       {:#?}", engine_type);
+            } else {
+                // コマンドライン引数をそのまま表示する。
+                println!(
+                    " {} {} {:?} {:?}",
+                    name, manufacturer, first_flight, engine_type
+                );
+            }
+        }
+        Commands::Idea { name, designer } => {
+            println!("{} {}", name, designer);
+        }
+    }
 }
