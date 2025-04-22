6a919458fa5a0b5561bd038c09f09ee1de087b2d
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-04-22 09:10:40 +0900

    パースを解析に変更

--- p0050-enum/src/main.rs
+++ p0050-enum/src/main.rs
@@ -1,10 +1,10 @@
-// clapクレートを使ってコマンドライン引数をパースするプログラム。
+// clapクレートを使ってコマンドライン引数を解析（パース）するプログラム。
 // Cargo.tomlに以下を追加する。
 // [dependencies]
 // clap = { version = "4.0", features = ["derive"] }
 // clapのderive機能を使うために、クレートのバージョンを4.0以上にする必要がある。
 
-// clapクレートは、コマンドライン引数をパースするためのライブラリ。
+// clapクレートは、コマンドライン引数を解析するためのライブラリ。
 use clap::{Parser, ValueEnum};
 
 // enum型のコマンドライン引数を定義する。
@@ -16,11 +16,11 @@ enum EngineType {
     Turbofan,
 }
 
-// derive(parser)属性を使ってコマンドライン引数のパースのためのコードを自動生成する。
+// derive(parser)属性を使ってコマンドライン引数の解析のためのコードを自動生成する。
 #[derive(Parser, Debug)]
 // command(version)属性を使って、コマンドラインに -V --version オプションを追加する。
 #[command(version)]
-// コマンドライン引数をパースするための構造体を定義する。
+// コマンドライン引数を解析するための構造体を定義する。
 // このプログラムではユーザー定義の構造体は空である。
 struct Cli {
     // 省略できないコマンドライン文字列。
@@ -31,19 +31,19 @@ struct Cli {
     #[clap(short, long, default_value = "")]
     manufacturer: String,
 
-    // 文字列以外のコマンドライン引数をパースする。
+    // 文字列以外のコマンドライン引数を解析する。
     #[clap(short, long, default_value_t = 1904)]
     first_flight_year: i32,
 
-    // enum型のコマンドライン引数をパースする。
+    // enum型のコマンドライン引数を解析する。
     #[clap(short, long, value_enum, default_value_t = EngineType::Reciprocating)]
     engine_type: EngineType,
 }
 
 fn main() {
-    // コマンドライン引数をパースする。
+    // コマンドライン引数を解析する。
     let cli = Cli::parse();
 
-    // コマンドライン引数をパースした結果を表示する。
+    // コマンドライン引数を解析した結果を表示する。
     println!("{:?}", cli);
 }
