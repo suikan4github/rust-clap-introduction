10e4392fc9f25d6a76f0de624ad051522e15121d
Seiichi Horie <26223147+suikan4github@users.noreply.github.com>
2025-04-23 22:01:53 +0900

    p0080 プログラム完成

--- p0080-shell-completion/src/main.rs
+++ p0080-shell-completion/src/main.rs
@@ -2,10 +2,10 @@
 // Cargo.tomlに以下を追加する。
 // [dependencies]
 // clap = { version = "4.0", features = ["derive"] }
-// clapのderive機能を使うために、クレートのバージョンを4.0以上にする必要がある。
 
 // clapクレートは、コマンドライン引数を解析するためのライブラリ。
-use clap::{Parser, ValueEnum};
+use clap::{CommandFactory as _, Parser, Subcommand, ValueEnum};
+use clap_complete;
 
 // enum型のコマンドライン引数を定義する。
 #[derive(Debug, Clone, ValueEnum)]
@@ -16,40 +16,114 @@ enum EngineType {
     Turbofan,
 }
 
+// derive(subcommand)属性を使って、コマンドライン引数の解析のためのコードを自動生成する。
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Real aircraft.
+    Real {
+        // 省略できないコマンドライン文字列。Docコメントはヘルプ情報に追加される。
+        #[arg()]
+        /// Name of aircraft.
+        name: String,
+
+        // 省略可能なコマンドライン文字列。
+        #[arg(short, long, default_value = "")]
+        /// Manufacturer of aircraft.
+        manufacturer: String,
+
+        // 文字列以外のコマンドライン引数を解析する。
+        #[arg(short, long, default_value_t = 1904)]
+        /// First flight year of aircraft.
+        first_flight: i32,
+
+        // enum型のコマンドライン引数を解析する。
+        #[arg(short, long, value_enum, default_value_t = EngineType::Reciprocating,
+         )]
+        /// Engine type of aircraft.
+        engine_type: EngineType,
+
+        // 論理型のコマンドライン引数を解析する。このオプションはスイッチとして機能する。
+        #[arg(short, long)]
+        /// Pretty print mode.
+        pretty_print: bool,
+    },
+    /// Idea only.
+    Idea {
+        // 省略できないコマンドライン文字列。
+        #[arg()]
+        /// Name of aircraft.
+        name: String,
+
+        // 省略可能なコマンドライン文字列。
+        #[arg(short, long, default_value = "")]
+        /// Designer of aircraft.
+        designer: String,
+    },
+    /// Generate shell completion script.
+    Complete {
+        /// Generate shell completion script.
+        #[arg(short, long, value_enum)]
+        shell: clap_complete::Shell,
+    },
+}
+
 // derive(parser)属性を使ってコマンドライン引数の解析のためのコードを自動生成する。
 #[derive(Parser, Debug)]
 // command(version)属性を使って、コマンドラインに -V --version オプションを追加する。
 #[command(version)]
 // コマンドライン引数を解析するための構造体を定義する。
-// このプログラムではユーザー定義の構造体は空である。
+// Doc コメントを使って、コマンドライン引数の説明をヘルプ情報に追加する。
+/// Demonstration of command/subcommand.
+///
+/// By using `#[command(subcommand)]`, you can define subcommands.
+/// This program demonstrates how to use subcommands with clap.
+///
+/// To see the detail of the each command, try `aircraft <COMMAND> -h`.
 struct Cli {
-    // 省略できないコマンドライン文字列。
-    #[arg(help = "Name of airclaft")]
-    name: String,
-
-    // 省略可能なコマンドライン文字列。
-    #[arg(short, long, default_value = "", help = "Manufacturer of airclaft")]
-    manufacturer: String,
-
-    // 文字列以外のコマンドライン引数を解析する。
-    #[arg(
-        short,
-        long,
-        default_value_t = 1904,
-        help = "First flight year of airclaft"
-    )]
-    first_flight: i32,
-
-    // enum型のコマンドライン引数を解析する。
-    #[arg(short, long, value_enum, default_value_t = EngineType::Reciprocating,
-        help = "Engine type")]
-    engine_type: EngineType,
+    // コマンドライン引数のサブコマンドを定義する。
+    #[command(subcommand)]
+    /// Subcommand of aircraft.
+    command: Commands,
 }
 
 fn main() {
     // コマンドライン引数を解析する。
     let cli = Cli::parse();
 
-    // コマンドライン引数を解析した結果を表示する。
-    println!("{:?}", cli);
+    match cli.command {
+        Commands::Real {
+            name,
+            manufacturer,
+            first_flight,
+            engine_type,
+            pretty_print,
+        } => {
+            if pretty_print {
+                // コマンドライン引数をきれいに表示する。
+                println!("Name         {:#?}", name);
+                println!("Manufacturer {:#?}", manufacturer);
+                println!("First flight {:#?}", first_flight);
+                println!("Engine       {:#?}", engine_type);
+            } else {
+                // コマンドライン引数をそのまま表示する。
+                println!(
+                    " {}, {}, {}, {:?}",
+                    name, manufacturer, first_flight, engine_type
+                );
+            }
+        }
+        Commands::Idea { name, designer } => {
+            println!("{}, {}", name, designer);
+        }
+        Commands::Complete { shell } => {
+            // 補完ファイルを生成する。
+            let mut cmd = Cli::command();
+            clap_complete::generate(
+                shell,
+                &mut cmd,
+                env!("CARGO_PKG_NAME"),
+                &mut std::io::stdout(),
+            );
+        }
+    }
 }
